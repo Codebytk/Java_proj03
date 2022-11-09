5e34e43f876740c8563674ac094e11a89c643a5c
Codebytk <ltk37733956@gmail.com>
2022-11-09 11:40:15 +0900

    첫번째 커밋

---
+++ src/chapter01/ex01/Helloworld.java
@@ -0,0 +1,34 @@
+package chapter01.ex01;
+
+public class Helloworld {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 주석 : 한 라인 주석 입니다.
+			// 주석은 프로그램에서 실행되지 않는 부분입니다.
+		/*
+		 * 여려 라인 주석입니다.
+		 */
+		System.out.println("출력 입니다.");
+		
+		System.out.println("출력 2 입니다.");
+		System.out.println("출력 3 입니다.");
+			//syso + <ctrl+spacebar>
+		//Helloworld 출력하기
+				System.out.println("Helloworld");
+				
+				//crtl+s : 저장
+				//F11
+				
+				//이클립스에서는 프로그램을 작성하고 저장하는 순간 자동으로 컴파일이됨
+				//Build(빌드) : 작성한 프로그램을 컴파일
+				//	Java_Test_Project\src : 프로그램 소스 코드가 저장되는 경로(*.java)
+				//	Java_Test_Projext\bin : 바이트 코드(중간언어)(*,class)
+				//	Java는 컴파일을 하면 완전한 기계어가 아닌 중간언어(바이트 코드)로 컴파일 된다.
+				//	- JVM이 완전한 기계어로 컴파일을 시켜줌
+				//	- JVM이 설치된 모든 곳에서 실행이 가능함
+				//	- 플렛폼에 종속적이지 않다.(h/w(cpu),os)
+
+	}
+
+}
